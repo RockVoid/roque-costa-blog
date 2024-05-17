835cd23484cd026fa9de3ea00770bc6c1363f911
Roque Costa <devroque18@gmail.com>
2024-05-17 15:40:16 -0300

    [feat]. creating a provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet
+} from "react-router-dom";
+
+const Layout = () => {
+  return (<h1>Layout</h1>);
+}
+
 function App() {
     return (
       <div className="app">
         <div className="container">
-            Hello again, JS :D
+          Hello again, JS :D
         </div>
       </div>
     );
