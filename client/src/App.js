415f97877197ccbabd30c41ea3fce85ad857051b
Roque Costa <devroque18@gmail.com>
2024-05-17 13:48:37 -0300

    [chor]. client setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div className="app">
+        <div className="container">
+            Hello again, JS :D
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
